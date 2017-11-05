89931c78bdf30f7b5c06a3201c3d2696f8151176
Sébastien Erfani <sebastien.erfani@gmail.com>
2017-11-05 12:58:01 +0100

    Add initial sass, add functional cookie disclaimer

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery3
 //= require rails-ujs
+
+//= require_directory .
+
 //= require turbolinks
-//= require_tree .
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def site_name
+        "Site name"
+    end
+
+    # TODO Change color 
+    def accent
+        "#000000"
+    end
 end
