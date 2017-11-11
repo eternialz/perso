1c4bf9985b00dcf0806e7187d84695c09b196412
Sébastien Erfani <sebastien.erfani@gmail.com>
2017-11-11 16:36:10 +0100

    Added articles controller

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,63 @@
+class ArticlesController < ActionController::Base
+    require 'pandoc-ruby', only: [:show]
+    before_action :set_article, except: [:new, :create]
+    before_action :check_api_key, except: [:show]
+
+    def index
+        @articles = Article.all
+    end
+
+    def show
+        @content = PandocRuby.convert(@article.content, :from => :markdown, :to => :html)
+    end
+
+    def new
+        @article = Article.new
+    end
+
+    def create
+        @article = Article.new
+        process_article
+    end
+
+    def edit
+        process_article
+    end
+
+    def update
+        process_article
+    end
+
+    def destroy
+        @article.destroy
+        flash[:success] = "Article supprimé"
+    end
+
+    private
+    def article_params
+        params.require(:article).permit(:title, :content)
+    end
+
+    def set_article
+        @article = Article.find(params[:id])
+    end
+
+    def check_api_key
+        unless params[:api] == ENV['BLOG_API_KEY']
+            flash[:error] = "La clé API fournie n'est pas correcte"
+            redirect_to: blog_index_path
+        end
+    end
+
+    def process_article
+        @article.assign_attributes(article_params)
+
+        if @article.save?
+            flash[:success] = "Success"
+        else
+            flash[:error] = "Erreur"
+        end
+
+        redirect_to blog_index_path
+    end
+end
